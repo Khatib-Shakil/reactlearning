4d825adcc062e02380746408e69ee2009235c9a6
Khatib-Shakil <shakil.khatib@yudiz.com>
2023-06-08 13:33:03 +0530

    add: integrate signup API

--- fornt-end/src/App.js
+++ fornt-end/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<h1>Product Listing Components</h1>} />
           <Route path="/add" element={<h1>Add Product Components</h1>}/>
           <Route path="/update" element={<h1>update Product Components</h1>}/>
-          <Route path="/logout" element={<h1>Logout</h1>}/>
+          <Route path="/logout" element={<h1>Logout</h1>}/> 
           <Route path="/profile" element={<h1>Profile</h1>}/>
           <Route path="/signup" element={<h1>SignUp</h1>}/>
      </Routes>
--- fornt-end/src/components/SignUp.js
+++ fornt-end/src/components/SignUp.js
@@ -1,35 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./SignUp.css";
 
-const SignUp =()=>{
+const SignUp = () => {
 
-    const[name,setName]=useState("");
-    const[email,setEmail]=useState("");
-    const[password,setPassword]=useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const setUserName=(event)=>{
-        setName(event.target.value);
+    const collectData = async () => {
+        console.log(name, email, password);
+        let result = await fetch('http://localhost:4000/register', {
+            method: "POST",
+            body: JSON.stringify({ name, email, password }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        result = await result.json()
+        console.log(result);
+
+        setName("");
+        setEmail("");
+        setPassword("");
     }
 
-    const setUserEmail=(event)=>{
-        setEmail(event.target.value);
-    }
 
-    const setUserPassword=(event)=>{
-        setPassword(event.target.value);
-    }
-
-    const handleSubmit=(event)=>{
-    }
-
-
-    return(
+    return (
         <div className='register'>
             <h1>Register</h1>
-            <input className="inputbox" type="text" placeholder="Enter Name" value={setUserName}></input>
-            <input className="inputbox" type="text" placeholder="Enter Email" value={setUserEmail}></input>
-            <input className="inputbox" type="password" placeholder = "Enter Password" value={setPassword}></input>
-            <button className="appbutton" type="button">Sign Up</button>
+            <input className="inputbox" type="text" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} />
+            <input className="inputbox" type="text" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input className="inputbox" type="password" maxLength="8" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={collectData} className="appbutton" type="button">Sign Up</button>
         </div>
     )
 }
